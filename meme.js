47753cef763b9620e2c7e6714a47a54cd6246ada
qiantianzhou <43750963+qiantianzhou@users.noreply.github.com>
2019-10-16 22:53:02 -0400

    Add files via upload

--- meme.js
+++ meme.js
@@ -1,28 +1,151 @@
 let img;
 
+
 function setup(){
 
-	var cnv = createCanvas(windowWidth, windowHeight);
+	var cnv = createCanvas(1200, 670);
 	cnv.style('display',"block");
 	cnv.parent('content');
 	img = loadImage('meme.jpg');
 }
 
 function draw() {
-//	background(119,119,119);
+
+
+    
 	noStroke();
-	image(img,0,0,width,height);
+	image(img,0,0,1200,670);
     
     push();
-    translate(1025,260);
+    translate(762,179);
     fill(255,255,255);
     ellipse(0,0,100,80);
+    fill(0,0,0);
+    ellipse(map(mouseX, 0, width,-10,10),0,30,30);
     pop();
     
     push();
-    translate(1200,310);
+    translate(900,217);
     fill(255,255,255);
     ellipse(0,0,100,80);
+    fill(0,0,0);
+    ellipse(map(mouseX, 0, width,-10,10),0,30,30);
+    pop();
+    
+    push();
+    
+    translate(width/2,height/2);
+    rotate(frameCount*PI/120);
+
+    translate(200,60);
+
+    
+    
+    fill(255,0,0);
+    ellipse(0,0,60,60);
+    beginShape();
+    vertex(0,0);
+    vertex(-30,0);
+    vertex(-40,-30);
+    endShape(close);
+    beginShape();
+    vertex(0,0);
+    vertex(30,0);
+    vertex(40,-30);
+    endShape(close);
+    beginShape();
+    vertex(-30,0);
+    vertex(30,0);
+    vertex(0,-70);
+    endShape(close);
+    
+    pop();
+    
+  push();
+    
+    translate(width/2,height/2);
+    rotate(frameCount*PI/100);
+    translate(300,60);
+
+    
+    
+    fill(255,0,0);
+    ellipse(0,0,60,60);
+    beginShape();
+    vertex(0,0);
+    vertex(-30,0);
+    vertex(-40,-30);
+    endShape(close);
+    beginShape();
+    vertex(0,0);
+    vertex(30,0);
+    vertex(40,-30);
+    endShape(close);
+    beginShape();
+    vertex(-30,0);
+    vertex(30,0);
+    vertex(0,-70);
+    endShape(close);
+    
+    pop();
+    
+    
+    push();
+    
+    translate(width/2,height/2);
+    rotate(frameCount*PI/100);
+    translate(100,60);
+
+    
+    
+    fill(255,0,0);
+    ellipse(0,0,60,60);
+    beginShape();
+    vertex(0,0);
+    vertex(-30,0);
+    vertex(-40,-30);
+    endShape(close);
+    beginShape();
+    vertex(0,0);
+    vertex(30,0);
+    vertex(40,-30);
+    endShape(close);
+    beginShape();
+    vertex(-30,0);
+    vertex(30,0);
+    vertex(0,-70);
+    endShape(close);
+    
+    pop();
+    push();
+    
+    translate(width/2,height/2);
+    rotate(frameCount*PI/100);
+    translate(500,60);
+
+    
+    
+    fill(255,0,0);
+    ellipse(0,0,60,60);
+    beginShape();
+    vertex(0,0);
+    vertex(-30,0);
+    vertex(-40,-30);
+    endShape(close);
+    beginShape();
+    vertex(0,0);
+    vertex(30,0);
+    vertex(40,-30);
+    endShape(close);
+    beginShape();
+    vertex(-30,0);
+    vertex(30,0);
+    vertex(0,-70);
+    endShape(close);
+    
+    pop();
+    
+    
     
 
 
